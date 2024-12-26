76246f07db8ceb406602b551cf275027051dc252
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-12-26 16:05:27 +0500

    @icons

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,10 @@
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import '../globals.css';
 import type { Metadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import StoreProvider from '@/store/StoreProvider';
+import '../globals.css';
 
 export const metadata: Metadata = {
   title: 'Med Yordam',
@@ -27,12 +27,21 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
-        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" sizes="180x180" />
-        <link rel="icon" href="/favicon/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
+
+        <meta property="og:title" content="Med Yordam" />
+        <meta property="og:description" content="Ваш надежный онлайн-помощник в сфере здравоохранения. Получите доступ к консультациям врачей, полезным советам по здоровью и проверенным медицинским ресурсам." />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:image:type" content="favicon-96x96.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="https://med-yordam-eight.vercel.app/ru" />
+        <meta property="og:type" content="website" />
       </head>
       <body>
         <StoreProvider>
