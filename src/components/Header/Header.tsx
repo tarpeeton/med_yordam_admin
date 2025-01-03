7ed435b35fa6c923b875299855b381e9f4d2b8d2
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2025-01-04 02:32:38 +0500

    phone  , link header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {  FC, useState } from "react";
+import { FC, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { SlLocationPin } from "react-icons/sl";
 import { RiArrowDownSLine } from "react-icons/ri";
@@ -8,6 +8,7 @@ import { Link } from '@/i18n/routing';
 import LanguageSwitcher from '@/components/Header/LanguageSwither';
 import MobileMenu from '@/components/Header/MobileMenu';
 import LogoDefault from '../../public/menu/Logo.svg'
+import { usePathname } from 'next/navigation';
 
 
 
@@ -21,52 +22,70 @@ import LogoDefault from '../../public/menu/Logo.svg'
 
 const Header: FC = () => {
   const locale = useLocale()
-  const [menu , setMenu] = useState(false)
-
+  const [menu, setMenu] = useState(false)
+  const pathname = usePathname();
   const SwitcherMenuMobile = () => setMenu(!menu)
 
-    return (
-      <header >
-        <div className='flex flex-row justify-between items-center h-[59px] px-[16px] slg:px-[20px] 2xl:px-[60px] 3xl:px-[100px]'>
-        <button className='flex flex-row items-center flex-grow-[1]'>
-            <SlLocationPin className='mr-[4px] mdl:mr-[8px] text-[#0129E3] w-[16px] h-[16px] '/>
-              <p className='text-[15px] slg:text-[16px] '>
-                {locale === 'ru' ? 'Ташкент' : locale === 'uz' ? 'Toshkent' : 'Tashkent'}
-              </p>
-              <p className='ml-[8px] w-[16px] h-[16px] '>
-                <RiArrowDownSLine className='w-full h-full text-[#747474]'/>
-              </p>
-          </button>
-        {/* LANGUAGE SWITCHER */}
-          <LanguageSwitcher />
 
-          {/* MENU */}
-          <button  onClick={SwitcherMenuMobile} className='w-[24px] h-[24px]  mdl:hidden'>
-        <RxHamburgerMenu className='text-[#000000] w-full h-full' />
-        {menu && <MobileMenu closeMenu={SwitcherMenuMobile } />}
-      </button>
-        </div>
-         
-        <div className='px-[16px] slg:px-[20px] 2xl:px-[60px] 3xl:px-[100px]'>
+
+
+  return (
+    <header >
+      <div className='flex flex-row justify-between items-center h-[59px] px-[16px] slg:px-[20px] 2xl:px-[60px] 3xl:px-[100px]'>
+        <button className='flex flex-row items-center flex-grow-[1]'>
+          <SlLocationPin className='mr-[4px] mdl:mr-[8px] text-[#0129E3] w-[16px] h-[16px] ' />
+          <p className='text-[15px] slg:text-[16px] '>
+            {locale === 'ru' ? 'Ташкент' : locale === 'uz' ? 'Toshkent' : 'Tashkent'}
+          </p>
+          <p className='ml-[8px] w-[16px] h-[16px] '>
+            <RiArrowDownSLine className='w-full h-full text-[#747474]' />
+          </p>
+        </button>
+        {/* LANGUAGE SWITCHER */}
+        <LanguageSwitcher />
+
+        {/* MENU */}
+        <button onClick={SwitcherMenuMobile} className='w-[24px] h-[24px]  mdl:hidden'>
+          <RxHamburgerMenu className='text-[#000000] w-full h-full' />
+          {menu && <MobileMenu closeMenu={SwitcherMenuMobile} />}
+        </button>
+      </div>
+
+      <div className='px-[16px] slg:px-[20px] 2xl:px-[60px] 3xl:px-[100px]'>
         <div className='h-[79px] px-[20px] mdl:flex items-center justify-between flex-row flex-nowrap hidden'>
-        <div>
-          <img src={LogoDefault.src} alt="" width={300} height={90} className='w-full h-full object-cover' />
-        </div>
-        <div className='flex flex-row gap-[8px] mdl:w-[50%] slg:w-[40%]'>
-          <button className='bg-white py-[17px] w-[50%] rounded-tl-[20px] rounded-bl-[20px] flex items-center justify-center text-[16px] '>
-            {locale === 'ru' ? " О проекте" : locale === 'uz' ? 'Loyiha haqida' : locale === 'en' ? 'About the project' : "About the project"}
-          </button>
-          <Link href='/register' className='bg-white py-[17px] w-[50%] rounded-tr-[20px] rounded-br-[20px] flex items-center justify-center text-[16px] text-[#0129E3]'>
-            {locale === 'ru' ? "Регистрация" : locale === 'uz' ? 'Ro\'yhatdan o\'tish' : 'Register'}
-          </Link>
+          <div>
+            <img src={LogoDefault.src} alt="" width={300} height={90} className='w-full h-full object-cover' />
+          </div>
+          <div className='flex flex-row gap-[8px] mdl:w-[50%] slg:w-[40%]'>
+            <button className='bg-white py-[17px] w-[50%] rounded-tl-[20px] rounded-bl-[20px] flex items-center justify-center text-[16px] '>
+              {locale === 'ru' ? " О проекте" : locale === 'uz' ? 'Loyiha haqida' : locale === 'en' ? 'About the project' : "About the project"}
+            </button>
+            <Link
+              href={
+                pathname === `/${locale}/register` ? "/login" : "/register"
+              }
+              className="bg-white py-[17px] w-[50%] rounded-tr-[20px] rounded-br-[20px] flex items-center justify-center text-[16px] text-[#0129E3]"
+            >
+              {pathname === `/${locale}/register`
+                ? locale === "ru"
+                  ? "Войти"
+                  : locale === "uz"
+                    ? "Kirish"
+                    : "Login"
+                : locale === "ru"
+                  ? "Регистрация"
+                  : locale === "uz"
+                    ? "Ro'yhatdan o'tish"
+                    : "Register"}
+            </Link>
+          </div>
         </div>
       </div>
-        </div>
 
 
-      
-      </header>
-    )
+
+    </header>
+  )
 }
 
 
