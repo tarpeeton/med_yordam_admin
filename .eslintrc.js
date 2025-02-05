475da8ad861ef7f222e03de0db72beec34973f07
tarpeeton <tarpeetonteam@gmail.com>
2025-02-06 12:28:13 +1400

    husky

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
