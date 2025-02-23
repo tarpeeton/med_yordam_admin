bb2d79759429d805b66a5b392f9f2163e70915eb
tarpeeton <tarpeetonteam@gmail.com>
2025-02-24 11:52:49 +1400

    mobile menu fixed

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -1,49 +1,57 @@
-"use client";
-import { FC } from "react";
-import { IoClose } from "react-icons/io5";
+'use client';
+import { FC } from 'react';
+import { IoClose } from 'react-icons/io5';
 import { useLocale } from 'next-intl';
-import {Link} from "@/i18n/routing";
+import { Link } from '@/i18n/routing';
 import Image from 'next/image';
-import LogoDefault from '../../public/menu/Logo.svg'
-
-
-
-
+import LogoDefault from '../../public/menu/Logo.svg';
 
 interface MobileMenuProps {
-  closeMenu: () => void
+  closeMenu: () => void;
 }
 
 const MobileMenu: FC<MobileMenuProps> = ({ closeMenu }) => {
-  const locale = useLocale()
-
+  const locale = useLocale();
 
   return (
-    <div className="fixed inset-0 z-10 bg-[#00000080]"> 
-     <div className="absolute w-[80%] right-0 bg-[#F6FAFF] top-0 z-10 h-[495px]">
-      <div className="flex items-center justify-end h-[59px]">
-        <button onClick={closeMenu} className="w-[24px] h-[24px] mr-[20px]">
-          <IoClose className="text-titleDark w-full h-full" />
-        </button>
-      </div>
-      {/* Add more menu content here */}
-      <div className='mt-[41px] px-[41px] w-full'>
-        <Image src={LogoDefault} alt="" width={300} height={90} quality={100} className='w-full h-full object-cover' />
-        <div className='mt-[31px] flex flex-col gap-[12px]'>
-          <button className='py-[15px] min-w-[120px] text-[14px] rounded-full bg-white font-medium'>
-           
-            {locale === 'ru' ? " О проекте": locale ==='uz' ? 'Loyiha haqida': locale === 'en' ? 'About the project' : "About the project"}
-            </button>
-          <Link href='/register' className='py-[15px] min-w-[120px] text-[14px] rounded-full bg-white text-[#0129E3] font-medium'>
-
-          {locale === 'ru' ? "Регистрация": locale ==='uz' ? 'Ro\'yhatdan o\'tish':  'Register' }
-
-          </Link>
+    <div className="fixed inset-0 z-10 bg-[#00000080]">
+      <div className="absolute right-0 top-0 z-10 h-[495px] w-[80%] bg-[#F6FAFF]">
+        <div className="flex h-[59px] items-center justify-end">
+          <button onClick={closeMenu} className="mr-[20px] h-[24px] w-[24px]">
+            <IoClose className="h-full w-full text-titleDark" />
+          </button>
+        </div>
+        {/* Add more menu content here */}
+        <div className="mt-[41px] w-full px-[41px]">
+          <Image
+            src={LogoDefault}
+            alt=""
+            width={300}
+            height={90}
+            quality={100}
+            className="h-full w-full object-cover"
+          />
+          <div className="mt-[31px] flex flex-col gap-[12px]">
+            <Link
+              href={'/login'}
+              className="min-w-[120px] rounded-full bg-white py-[15px] text-[14px] font-medium"
+            >
+              {locale === 'ru' ? 'Вход' : locale === 'uz' ? 'Kirish' : 'Login'}
+            </Link>
+            <Link
+              href="/register"
+              className="min-w-[120px] rounded-full bg-white py-[15px] text-center text-[14px] font-medium text-[#0129E3]"
+            >
+              {locale === 'ru'
+                ? 'Регистрация'
+                : locale === 'uz'
+                  ? "Ro'yhatdan o'tish"
+                  : 'Register'}
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-   
   );
 };
 
