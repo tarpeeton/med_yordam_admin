aabdeadea1c113291026958fac9903415bc6ea7e
tarpeeton <tarpeetonteam@gmail.com>
2025-02-27 10:26:46 +1400

    succes to login register

--- src/components/Modals/successModal.tsx
+++ src/components/Modals/successModal.tsx
@@ -1,58 +1,67 @@
-'use client'
-import {FC} from 'react'
+'use client';
+import { FC } from 'react';
 import { Link } from '@/i18n/routing';
 import Image from 'next/image';
-import { useRouter } from "next/router";
 
 // SUCCES Image
-import Success from '@/public/modal/success.png'
+import Success from '@/public/modal/success.png';
 import { useLocale } from 'next-intl';
 
 interface IModalProps {
-  open: boolean
-  close: () => void
-  title: {ru: string , uz:string , en:string},
-  subtitle: {ru: string , uz:string , en:string},
+  open: boolean;
+  close: () => void;
+  title: { ru: string; uz: string; en: string };
+  subtitle: { ru: string; uz: string; en: string };
 }
 
-
-export const SuccessModal: FC<IModalProps> = ({ open, close , title , subtitle }) => {
+export const SuccessModal: FC<IModalProps> = ({
+  open,
+  close,
+  title,
+  subtitle,
+}) => {
   const locale = useLocale() as 'ru' | 'uz' | 'en';
-  
- // Prevent the click event from propagating to the overlay
- const handleContentClick = (e: React.MouseEvent) => {
-  e.stopPropagation();
-};
 
-if (!open) return null; // Do not render when modal is closed
+  // Prevent the click event from propagating to the overlay
+  const handleContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  if (!open) return null; // Do not render when modal is closed
 
   return (
-    <div onClick={close} className="fixed inset-0 z-50 bg-[#050B2B] bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-      <div className="py-[30px] px-[24px] w-[90%] 2xl:w-[690px] relative overflow-hidden bg-gradient-to-tl from-[#379FF2] to-[#022FE4] shadow-lg rounded-[30px] ">
-        <div className='absolute top-[-5px] right-[-5px] '>
-          <Image src={Success} alt="Image One" width={300} height={300} quality={100} className='w-full h-full object-cover' />
+    <div
+      onClick={close}
+      className="fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-[#050B2B] bg-opacity-50"
+    >
+      <div className="relative w-[90%] overflow-hidden rounded-[30px] bg-gradient-to-tl from-[#379FF2] to-[#022FE4] px-[24px] py-[30px] shadow-lg 2xl:w-[690px]">
+        <div className="absolute right-[-5px] top-[-5px]">
+          <Image
+            src={Success}
+            alt="Image One"
+            width={300}
+            height={300}
+            quality={100}
+            className="h-full w-full object-cover"
+          />
         </div>
-        <div className='2xl:flex 2xl:flex-col 2xl:items-center'>
-        <h3 className="text-[30px] 2xl:text-center slg:text-[45px] 2xl:text-[50px] text-left text-white font-medium">
-          {title[locale]}
+        <div className="2xl:flex 2xl:flex-col 2xl:items-center">
+          <h3 className="text-left text-[30px] font-medium text-white slg:text-[45px] 2xl:text-center 2xl:text-[50px]">
+            {title[locale]}
           </h3>
-         
-         <p className='mt-[16px]  2xl:w-[60%] text-white 2xl:text-center text-left font-medium text-[15px] slg:text-[16px] 2xl:text-[16.5px]'>
-         {subtitle[locale]}
-         </p>
-        
+
+          <p className="mt-[16px] text-left text-[15px] font-medium text-white slg:text-[16px] 2xl:w-[60%] 2xl:text-center 2xl:text-[16.5px]">
+            {subtitle[locale]}
+          </p>
         </div>
-        <div className='mt-[30px] slg:mt-[35px]'>
-         <Link  href={'/dashboard'}  className='w-full  h-full '>
-         <p className='text-[14px] w-full h-[55px] font-medium slg:text-[15px] rounded-[12px] flex items-center justify-center bg-white  text-[#202020] 2xl:h-[76px] 2xl:text-[17px]'>
-         Продолжить
-
-         </p>
-         </Link>
-         </div>
-      
-
+        <div className="mt-[30px] slg:mt-[35px]">
+          <Link href={'/login'} className="h-full w-full">
+            <p className="flex h-[55px] w-full items-center justify-center rounded-[12px] bg-white text-[14px] font-medium text-[#202020] slg:text-[15px] 2xl:h-[76px] 2xl:text-[17px]">
+              Продолжить
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
-}
+};
